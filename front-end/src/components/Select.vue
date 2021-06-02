b4db995dd857b28c76b5c97dfa02df79215c1aec
Emmanuel López Laguna <emmlagunahrx@gmail.com>
2021-06-02 17:38:19 -0600

    Agregando componente select para formularios

---
+++ front-end/src/components/Select.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+      <label :for="id">{{titulo}}</label>
+      <select 
+        :id="id"
+        class="form-control"
+        :value="value"
+        :disabled="disabled"
+        @input="$emit('input', $event.target.value)"
+        >
+        <option 
+            value="" selected disabled hidden
+        >
+        {{placeholder}}
+        </option>
+        <option 
+            v-for="(option, i) in options" :key="'option' + i" 
+            :value="option.value" 
+        >
+        {{option.text}}
+        </option>
+      </select>
+        <span v-if="error">{{ mensajeError }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+        options: [],
+        id: {
+            type: [Number, String],
+            required: true
+        },
+        titulo: String,
+        value: [Number, String],
+        tipo: {
+            type: String,
+            default: 'text'
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        placeholder: {
+            type: String,
+            default: 'Seleccione'
+        },
+        mensajeError: {
+            type: String,
+            default: 'Seleccione una opción válida'
+        },
+        error: {
+            type: Boolean,
+            default: false
+        }
+    }
+  }
+</script>
