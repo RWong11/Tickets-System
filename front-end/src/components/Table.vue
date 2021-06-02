f4fdec6df1824bff70708155168af2a7b06a2aeb
Emmanuel López Laguna <emmlagunahrx@gmail.com>
2021-06-02 04:55:54 -0600

    Agregando Tabla genérica como componente

---
+++ front-end/src/components/Table.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <b-table stripped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Table',
+    props: {
+        items: Array
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- front-end/src/views/Tickets.vue
+++ front-end/src/views/Tickets.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="Tickets">
-      Tickets
+      Tickets 
+    <Table :items='[{Nombre:"A", Edad:3}, {Nombre:"B", Edad:5}]'/>
   </div>
 </template>
 
 <script>
+import Table from '../components/Table.vue'
 
 export default {
   name: 'Tickets',
-  
+  components: {
+      Table
+  }
 }
 </script>
